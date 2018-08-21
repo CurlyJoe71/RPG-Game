17b4594032d223cec2c8ebd7b6acc7bd8b41c515
Curly Joe <curlyjoe71@gmail.com>
2018-08-21 16:20:51 -0700

    Almost completed game files.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,152 @@
-$(document).ready(function() {
+var hero = '';
 
-    var hero;
+var currentEnemy = '';
 
-    var currentEnemy;
+var heroPlacement = $('.heroPlacement');
+var enemyPlacement = $('.enemyPlacement');
 
-    var attackPower = 0;
+var heroStats = {};
+var enemyStats = {};
 
-    var counterAttackPower = 0;
+var heroDivId = '';
+var currentEnemyDivId = '';
 
-    var currentPhase = 1;
+//initial stats for each character
+const character1Stats = { name: 'Akbar', ID: '#character1', HP: 100, attackPower: 20, counterAttackPower: 25, attackIncrement: 5 };
+const character2Stats = { name: 'Chewbacca', ID: '#character2', HP: 150, attackPower: 20, counterAttackPower: 9, attackIncrement: 13 };
+const character3Stats = { name: 'Boba Fett', ID: '#Character3', HP: 175, attackPower: 10, counterAttackPower: 3, attackIncrement: 6 };
+const character4Stats = { name: 'Palpatine', ID: '#character4', HP: 200, attackPower: 30, counterAttackPower: 10, attackIncrement: 7 };
 
-    var heroHealth = 100;
+//displaying initial HP stats for each character
+$('#character-1').find('span').text(character1Stats['HP']);
+$('#character-2').find('span').text(character2Stats['HP']);
+$('#character-3').find('span').text(character3Stats['HP']);
+$('#character-4').find('span').text(character4Stats['HP']);
 
-    var enemyHP = 100;
+//need to somehow reassign the character object to a new variable to be used in functions
 
-    // need function to calculate damage
-    shotsFired = function() {
-        // $('#heroHP').text(100);
-        console.log(heroHealth);
-        $('span').text(heroHealth);
-    };
+var attackFunction = function () {
+    var newHeroHP = heroStats['HP'] - enemyStats['counterAttackPower'];
+    var newEnemyHP = enemyStats['HP'] - heroStats['attackPower'];
+    heroStats['HP'] = newHeroHP;
+    enemyStats['HP'] = newEnemyHP;
+    console.log('the new Hero HP is :' + newHeroHP);
+    $(heroDivId).find('span').text(newHeroHP);
+    $(currentEnemyDivId).find('span').text(newEnemyHP);
+    heroStats['attackPower'] += heroStats['attackIncrement'];
+    console.log('The hero\'s new attack power is: ' + heroStats['attackPower']);
+
+}
+
+var shotsFired = function () {
+    // $('#heroHP').text(100);
+    console.log(heroHealth);
+    $('span').text(heroHealth);
+};
+
+// $('.btn-attack').on('click', attackFunction);
+
+//I think I need an object that contains the details for each character. I can store their unique ID, and HP value, and whatever else I need to store
+// const charactersObject = [
+//     [character1: [
+
+//         name: 'Akbar',
+//         ID: "$('#character-1')",
+//         heroHealth: 100,
+//         counterAttackPower: 10]
+//     ],
+// ]
+
+//function to assign power
+
+// need function to calculate damage
 
 
-    console.log('Initial state of hero is: ' + hero);
-   
-    $('.character').on('click', function() {
 
-        if (currentPhase === 1) {
-            $(this).css({'background': 'yellow'});
+console.log('Initial state of hero is: ' + hero);
+
+$(document).ready(function () {
+
+    $('.character').on('click', function () {
+        if (hero === '') {
+            $(this).css({ 'background': 'yellow' });
             hero = $(this).attr('id');
-            currentPhase ++;
+            heroDivId = '#' + hero;
             console.log('The current hero is: ' + hero);
-            //add way to move other characters, maybe use append?
+            console.log('The heroDivId is: ' + heroDivId);
+
+            $(heroPlacement).append($(heroDivId));
+            $(heroDivId).find('span').css({ 'color': 'red'});
+            $('#ptag').text('Select your first enemy.');
+
+            //selecting the other character divs
+            $('.character').not($(this)).each(function () {
+                $(this).css({ 'background': 'purple' });
+            });
             
-        } else if (currentPhase === 2) {
-            $(this).css({'background': 'red'});
+            switch (hero) {
+                case "character-1":
+                    heroStats = character1Stats;
+                    break;
+                case "character-2":
+                    heroStats = character2Stats;
+                    break;
+                case "character-3":
+                    heroStats = character3Stats;
+                    break;
+                case "character-4":
+                    heroStats = character4Stats;
+                }
+
+            //add way to move other characters, maybe use append?
+
+        } else if (currentEnemy === '') {
+            $('#ptag').text('Fight!');
+            $(this).css({ 'background': 'red' });
             currentEnemy = $(this).attr('id');
-            currentPhase++;
+            currentEnemyDivId = '#' + currentEnemy;
+            $(enemyPlacement).append($(currentEnemyDivId));
+
+            switch (currentEnemy) {
+                case "character-1":
+                    enemyStats = character1Stats;
+                    break;
+                case "character-2":
+                    enemyStats = character2Stats;
+                    break;
+                case "character-3":
+                    enemyStats = character3Stats;
+                    break;
+                case "character-4":
+                    enemyStats = character4Stats;
+                }
             console.log('The current enemy is: ' + currentEnemy);
 
-        } else if (currentPhase === 3){
-
+        } else {
         };
-            
-    });  //closes click event
-})      //closes document ready
+    }); //closes click event
+    
+    
+
+    $('.btn-attack').on('click', function() {
+        if (hero !== '' && currentEnemy !== '') {
+            attackFunction();
+        }
+        if (heroStats['HP'] <= 0) {
+            alert('You lost!')
+        } else if (enemyStats['HP'] <= 0) {
+            alert('You defeated the enmey!');
+            $(currentEnemyDivId).hide();
+            currentEnemy = '';
+            enemyStats = {};
+            currentEnemyDivId = '';
+            $(currentEnemyDivId).empty();
+            $('#ptag').text('Please select a new enemy to battle!');
+        }
+    })
+
+})  //closes click event
+   //closes document ready
 
 
 
